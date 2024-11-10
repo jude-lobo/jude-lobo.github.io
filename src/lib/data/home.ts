5dd5953e81a2bc6093945d13e0f8d781ef34866d
Jude Lobo <judejonathan.lobo@gmail.com>
2024-11-09 20:49:13 -0500

    change

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -15,7 +15,7 @@ export const description = 'specializing in building scalable digital solutions 
 // 	'An experienced Web Developer with 4 years of proficient experience in PHP Programming, specializing in Laravel, and CodeIgniter Frameworks. Seeking to leverage my expertise in JavaScript (JS), jQuery MySQL, and API Development to contribute to innovative web projects. Dedicated to optimizing web application performance and enhancing user experience through collaborative problem-solving and continuous learning of emerging technologies. Committed to delivering high-quality, scalable solutions while fostering teamwork and creativity in a dynamic work environment.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/jude-lobo' },
+	{ platform: Platform.GitHub, link: 'https://github.com/jude-lobo/' },
 	{
 		platform: Platform.Linkedin,
 		link: 'https://www.linkedin.com/in/jude-jonathan-lobo/'
