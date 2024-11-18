30e77eb9fd3efc4e29ea6586b4af677a08d94448
Jude Lobo <judejonathan.lobo@gmail.com>
2024-11-18 01:08:54 -0500

    Changed project colors

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -33,7 +33,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'unilever',
-		color: '#ff3e00',
+		color: '#1d4d8f',
 		description:
 			"<p>Unilever's website showcases its diverse range of products, brand information, and corporate initiatives. It offers an interactive platform for consumers to explore Unilever's offerings and engage with the brand.</p><h4>Responsibilities:</h4><ul><li>Contributed to various aspects of the website, implementing new features and enhancements.</li><li>Collaborated with the team to integrate marketing strategies and brand elements seamlessly into the website's design and functionality.</li></ul>",
 		shortDescription:
@@ -47,7 +47,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'knorr',
-		color: '#ff3e00',
+		color: '#92B800',
 		description:
 			"<p>Knorr's website features recipes, cooking tips, and product information related to Knorr's food products. It offers a platform for users to discover new recipes and engage with the Knorr brand.</p><h4>Responsibilities:</h4><ul><li>Designed and implemented various website features and modules.</li><li>Enhanced user engagement and brand visibility.</li></ul>",
 		shortDescription:
@@ -61,7 +61,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'lipton',
-		color: '#ff3e00',
+		color: '#F7D300',
 		description:
 			"<p>Lipton's website is focused on tea products, offering information about Lipton's tea varieties, recipes, and health benefits. It provides users with resources to explore tea-related content and promotions.</p><h4>Responsibilities:</h4><ul><li>Utilized AEM to create and integrate multiple modules.</li><li>Collaborated with design and marketing teams.</li></ul>",
 		shortDescription:
@@ -75,7 +75,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'jazz',
-		color: '#ff3e00',
+		color: '#FF003F',
 		description:
 			"<p>Jazz's website is a portal for telecom services, offering mobile plans, internet packages, and digital solutions. It provides users with access to account management tools and customer support.</p><h4>Responsibilities:</h4><ul><li>Managed backend portal and synchronized website.</li><li>Created and managed APIs for user transactions.</li></ul>",
 		shortDescription:
@@ -89,7 +89,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'hbl',
-		color: '#ff3e00',
+		color: '#002F6C',
 		description:
 			"<p>HBL's website offers a wide range of banking services, including loans, account management, and financial solutions. It provides users with easy access to banking tools and resources.</p><h4>Responsibilities:</h4><ul><li>Developed custom modules for loan calculation, form submissions, and data display.</li><li>Managed content and updates on the CMS.</li></ul>",
 		shortDescription:
@@ -103,7 +103,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'psx',
-		color: '#ff3e00',
+		color: '#006F9A',
 		description:
 			"<p>The PSX website serves as a hub for stock market analysis, trading, and information related to Pakistan's stock exchange. It provides tools for market analysis, glossaries for stock exchange terms, and a comprehensive gallery of stock-related content.</p><h4>Responsibilities:</h4><ul><li>Managed and optimized the backend logic using ExpressionEngine CMS, ensuring smooth operations and performance.</li><li>Conducted regular maintenance tasks, including bug fixing and performance tuning, to uphold the website's reliability and user experience.</li><li>Developed key features such as market analysis tools, a comprehensive glossary for stock exchange terms, an interactive gallery, and otfunctionalities essential to stock exchange operations.</li></ul>",
 		shortDescription:
@@ -117,7 +117,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'mcb',
-		color: '#ff3e00',
+		color: '#003B49',
 		description:
 			"<p>MCB's website is a comprehensive platform for banking services, including online banking, account management, and financial products. It provides users with tools to manage their finances efficiently.</p><h4>Responsibilities:</h4><ul><li>Managed backend features, maintenance, and development.</li><li>Developed and maintained specific backend functionalities for MCB.</li><li>Conducted rigorous testing and debugging.</li></ul>",
 		shortDescription:
@@ -131,7 +131,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'faysal',
-		color: '#ff3e00',
+		color: '#004F9F',
 		description:
 			"<p>Faysal Bank's website offers banking services, loan options, and financial solutions. It provides users with tools for online banking and account management.</p><h4>Responsibilities:</h4><ul><li>Worked on backend features critical to online banking.</li><li>Collaborated with stakeholders to gather requirements.</li></ul>",
 		shortDescription:
@@ -145,7 +145,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'iobm',
-		color: '#ff3e00',
+		color: 'blue',
 		description:
 			"<p>IoBM's website features information about the institute, academic programs, and student resources. It offers users tools for exploring educational opportunities and engaging with IoBM's community.</p><h4>Responsibilities:</h4><ul><li>Managed backend features, maintenance, and development related to IoBM's services.</li><li>Collaborated with IoBM to enhance backend functionalities, improving overall operational efficiency and user experience.</li></ul>",
 		shortDescription:
@@ -159,7 +159,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'ebm',
-		color: '#ff3e00',
+		color: '#F4A300',
 		description:
 			"<p>EBM's website features information about its food products, recipes, and nutritional content. It offers users a platform to discover EBM's offerings and engage with food-related content.</p><h4>Responsibilities:</h4><ul><li>Developed and maintained backend functionalities.</li><li>Conducted thorough testing and quality assurance.</li></ul>",
 		shortDescription:
@@ -173,7 +173,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'martindow',
-		color: '#ff3e00',
+		color: 'blue',
 		description:
 			"<p>MartinDow's website showcases its pharmaceutical products, research initiatives, and corporate information. It offers users resources to explore MartinDow's healthcare offerings.</p><h4>Responsibilities:</h4><ul><li>Led redesign and redevelopment efforts.</li><li>Conducted user testing and feedback sessions.</li></ul>",
 		shortDescription:
@@ -187,7 +187,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'searle',
-		color: '#ff3e00',
+		color: '#0C3C56',
 		description:
 			"<p>Searle's website features information about its pharmaceutical products, medical research, and healthcare services. It provides users with resources to learn about Searle's healthcare solutions.</p><h4>Responsibilities:</h4><ul><li>Managed backend features and integrations.</li><li>Implemented data management solutions.</li></ul>",
 		shortDescription:
@@ -201,7 +201,7 @@ export const items: Array<Project> = [
 	},
 	{
 		slug: 'khaadi',
-		color: '#ff3e00',
+		color: '#C72C41',
 		description:
 			"<p>Khaadi's website is focused on fashion products, offering clothing, accessories, and home decor. It provides users with a platform to shop for Khaadi's products and engage with fashion trends.</p><h4>Responsibilities:</h4><ul><li>Contributed to the development of specific website features.</li><li>Enhanced user engagement and sales efforts.</li></ul>",
 		shortDescription:
